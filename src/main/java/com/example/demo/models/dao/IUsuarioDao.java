9e074f32f7edf921c3319e8ee7f17acabd2a1185
Dasch11 <diego.suqui.est@tecazuay.edu.ec>
2025-12-10 21:52:40 -0500

    1

--- src/main/java/com/example/demo/models/dao/IUsuarioDao.java
+++ src/main/java/com/example/demo/models/dao/IUsuarioDao.java
@@ -4,6 +4,8 @@ import com.example.demo.models.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUsuarioDao extends JpaRepository<Usuario, Long> {
     boolean existsByEmail(String email);
